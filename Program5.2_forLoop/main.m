505ec3614da9ce2315a93698669cad7c64db858f
admin <dangtinhtong@gmail.com>
2017-11-09 13:00:35 +0700

    Initial Commit

---
+++ Program5.2_forLoop/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Program5.2_forLoop
+//
+//  Created by admin on 11/9/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
